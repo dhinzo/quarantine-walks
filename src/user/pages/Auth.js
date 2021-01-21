0de8c4ba8dca4bf91b007810a28efabe7cdec098
Devin Hinzo <devin.hinzo@gmail.com>
2021-01-21 18:17:18 -0500

    added error handling

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -4,13 +4,20 @@ import { VALIDATOR_EMAIL, VALIDATOR_MIN, VALIDATOR_REQUIRE } from '../../shared/
 import Card from '../../shared/components/UIElements/Card'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import { useForm } from '../../shared/hooks/form-hook'
 import { AuthContext } from '../../shared/context/auth-context'
 import './Auth.css'
 
 const Auth = () => {
+    
     const auth = useContext(AuthContext)
     const [isLoginMode, setIsLoginMode] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
+
+
     const [formState, inputHandler, setFormData] = useForm(
         {
             email: {
@@ -25,35 +32,61 @@ const Auth = () => {
         false
     )
 
-    const switchHandler = () => {
-        if (!isLoginMode) {
-            setFormData({
-                ...formState.inputs,
-                name: undefined    
-            }, formState.inputs.email.isValid && formState.inputs.password.isValid
-            )
-        } else {
-            setFormData({
-                ...formState.inputs,
-                name: {
-                    value: '',
-                    isValid: false
-                }
-            }, 
-            false
-            )
-        }
-        setIsLoginMode(prevMode => !prevMode)
+const switchHandler = () => {
+    if (!isLoginMode) {
+        setFormData({
+            ...formState.inputs,
+            name: undefined    
+        }, formState.inputs.email.isValid && formState.inputs.password.isValid
+        )
+    } else {
+        setFormData({
+            ...formState.inputs,
+            name: {
+                value: '',
+                isValid: false
+            }
+        }, 
+        false
+        )
     }
+    setIsLoginMode(prevMode => !prevMode)
+}
 
-    const authSubmitHandler = e => {
-        e.preventDefault()
-        console.log(formState.inputs)
+const authSubmitHandler = async e => {
+    e.preventDefault()
+    
+    if (isLoginMode) {
+
+    } else {
+        try{
+            setIsLoading(true)
+            const response = await fetch('http://localhost:5000/api/users/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: formState.inputs.name.value,
+                    email: formState.inputs.email.value,
+                    password: formState.inputs.password.value,
+            })
+        })
+        const resData = await response.json()
+        console.log(resData)
+        setIsLoading(false)
         auth.login()
-    }
+        } catch (err) {
+            console.log(err)
+            setIsLoading(false)
+            setError(err.message || "Something's not right... try again.")
+        }
+    }    
+}
 
     return (
         <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay/>}
         <h2>Login Required</h2>
         <hr />
         <form onSubmit={authSubmitHandler}>
